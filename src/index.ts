830e0cea4e138ef7428164684cbbfcad78c411f3
Tiago de Tarso <ttrgoncalves@gmail.com>
2023-10-27 20:15:08 -0300

    feat: seed cidades

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ const startServer = () => {
 if (process.env.IS_LOCALHOST !== 'true') {
     Knex.migrate.latest()
         .then(() => {
-            startServer()
+            Knex.seed.run()
+                .then(() => startServer())
+                .catch(console.log)
         })
         .catch(console.log)
 } else {
