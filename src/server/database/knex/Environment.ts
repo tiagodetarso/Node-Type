97d784ff229dc622923bc86c7ca08d2ccf62f55d
Tiago de Tarso <ttrgoncalves@gmail.com>
2023-11-02 19:17:06 -0300

    feat: added environment config for production

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -23,11 +23,33 @@ export const development: Knex.Config = {
     }
 }
 
+
 export const test: Knex.Config  = {
     ...development,
     connection: ':memory:',
 }
 
+
+export const production: Knex.Config  = {
+    client: 'pg',
+    migrations: {
+        directory: path.resolve(__dirname, '..', 'migrations')
+    },
+    seeds : {
+        directory: path.resolve(__dirname, '..', 'seeds')
+    },
+    connection: {
+        host: process.env.DB_HOST ,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT || 5432),
+        ssl: { rejectUnauthorized: false},
+    },
+}
+
+/*
 export const production: Knex.Config  = {
     ...development,
 }
+*/
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,6 +1,13 @@
 import { knex } from 'knex'
+import pg from 'pg'
+import 'dotenv/config'
+
 import { development, production, test } from './Environment'
 
+
+if (process.env.NODE_ENV === 'production') {
+    pg.types.setTypeParser(20, 'text', parseInt)
+}
 
 const getEnvironment = () => {
     switch (process.env.NODE_ENV) {
