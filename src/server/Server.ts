5980798c54dcae0e51da3b9f874ebc44bb926160
Tiago de Tarso <ttrgoncalves@gmail.com>
2024-04-23 18:02:51 -0300

    feat: some little changes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import 'dotenv/config'
 
 import './shared/services/TranslationsYup'
 import { router } from './routes'
 
 const server = express()
+
+server.use(cors({
+    origin: process.env.ENABLED_CORS?.split(';') || []
+}))
 
 server.use(express.json())
 
